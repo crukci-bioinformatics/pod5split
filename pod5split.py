71f6215636c03efa4cc2596571fd26d1fdad9b7b
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2024-04-04 12:22:00 +0100

    Still not working, but getting further.

--- pod5split.py
+++ pod5split.py
@@ -6,6 +6,7 @@ import sys
 
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
+from time import sleep
 from typing import Collection
 
 from tqdm.auto import tqdm
@@ -29,7 +30,7 @@ class Pod5Split():
                             description='Split a Pod5 file into smaller files of a specified number of reads.',
                             epilog='The default output directory is the current working directory.\nThe default name is the base name of the input file.')
         self.parser.add_argument('-r', '--reads', metavar = 'int', default = 25000, dest = 'chunkSize', type = int)
-        self.parser.add_argument('-i', '--in', metavar = 'file', dest = 'inBam', required = True, type = Path)
+        self.parser.add_argument('-i', '--in', metavar = 'file', dest = 'inPod5', required = True, type = Path)
         self.parser.add_argument('-o', '--out', metavar = 'dir', dest = 'outDir', type = Path)
         self.parser.add_argument('-b', '--base', metavar = 'name', dest = 'fileBase')
         # self.parser.add_argument('-t', '--threads', metavar = 'int', default = DEFAULT_THREADS, dest = 'threads', type = int)
@@ -39,24 +40,26 @@ class Pod5Split():
         if not self.outDir:
             self.outDir = Path(os.getcwd())
         if not self.fileBase:
-            self.fileBase = self.inBam.stem
+            self.fileBase = self.inPod5.stem
         if self.chunkSize < 1:
             self.chunkSize = 1
         #if self.threads < 1:
         #    self.threads = 1
 
+    @logged_all
     def split(self):
         if not self.outDir.exists():
             self.outDir.mkdir(exist_ok = True)
 
-        with pod5.Reader(self.inBam) as reader:
+        self.repacker = Repacker()
+
+        with pod5.Reader(self.inPod5) as reader:
             expectedChunks = (reader.num_reads + self.chunkSize - 1) // self.chunkSize
-            expectedKReads = (reader.num_reads + 999) // 1000
 
             pbar = tqdm(
-                total = expectedKReads,
+                total = reader.num_reads,
                 desc="Splitting",
-                unit="kRead",
+                unit="Read",
                 leave=True,
                 position=0,
                 **PBAR_DEFAULTS,
@@ -70,8 +73,8 @@ class Pod5Split():
                 chunkReadIds.append(pod5Record.read_id)
 
                 readNumber += 1
-                if readNumber % 1000 == 0:
-                    pbar.update(readNumber // 1000)
+                if readNumber % 100 == 0:
+                    pbar.update(readNumber)
 
                 if len(chunkReadIds) >= self.chunkSize:
 
@@ -84,26 +87,28 @@ class Pod5Split():
 
             if len(chunkReadIds) > 0:
                 self._writeChunk(reader, chunkNumber, readIds)
-                pbar.update(expectedKReads)
+                pbar.update(reader.num_reads)
 
-            pbar.close()
+        pbar.close()
+
+        #while not self.repacker.is_complete:
+        #    sleep(0.1)
+        sleep(0.5)
+
+        self.repacker.finish()
+
 
     def _writeChunk(self, reader: pod5.Reader, chunkNumber: int, readIds: Collection[str]):
+
         chunkName = f"{self.fileBase}_{chunkNumber:05d}.pod5"
         chunkPath = self.outDir / chunkName
 
         chunkPath.unlink(True)
 
-        repacker = Repacker()
-
         with pod5.Writer(chunkPath) as writer:
-            repackerOut = repacker.add_output(writer)
-
-            repacker.add_selected_reads_to_output(repackerOut, reader, readIds)
-            repacker.set_output_finished(repackerOut)
-
-        repacker.finish()
-        del repacker
+            repackerOut = self.repacker.add_output(writer)
+            self.repacker.add_selected_reads_to_output(repackerOut, reader, readIds)
+            self.repacker.set_output_finished(repackerOut)
 
 
 if __name__ == "__main__":
